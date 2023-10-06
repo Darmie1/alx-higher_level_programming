6851c8d74f5d46a72e15138c38a77bf0891167df
darmie1 <you@e>
2023-10-06 12:23:57 -0700

    easyprint

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
-    numbaofargs = len(args)
-    if numbaofargs == 0:
+    no_fargs = len(args)
+    if no_fargs == 0:
         print("0 arguments.")
     else:
-        print("{} argument{}:".format(numbaofargs, 's' if numbaofargs > 1 else ''))
+        print("{} argument{}:".format(no_fargs, 's' if no_fargs > 1 else ''))
         for i, arg in enumerate(args, 1):
             print("{}: {}".format(i, arg))
